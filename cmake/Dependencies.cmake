ab73248d341f43316eb4f0e30edb152e75eaca1d
Martin Helmut Fieber <info@martin-fieber.se>
2024-03-28 20:02:41 +0100

    Upgrade SDL and SDL_ttf to latest version

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -3,17 +3,19 @@ include(FetchContent)
 FetchContent_Declare(
   SDL2
   GIT_REPOSITORY "https://github.com/libsdl-org/SDL.git"
-  GIT_TAG release-2.28.4
+  GIT_TAG release-2.30.1
 )
-
-option(SDL2_DISABLE_SDL2MAIN "Disable building/installation of SDL2main" ON)
-FetchContent_MakeAvailable(SDL2)
 
 FetchContent_Declare(
   SDL2_ttf
   GIT_REPOSITORY "https://github.com/libsdl-org/SDL_ttf.git"
-  GIT_TAG release-2.20.2
+  GIT_TAG release-2.22.0
 )
 
+# For SDL2 to be able to override options
+set(CMAKE_POLICY_DEFAULT_CMP0077 NEW)
+
+set(SDL2_DISABLE_SDL2MAIN ON)
 set(SDL2TTF_INSTALL OFF)
-FetchContent_MakeAvailable(SDL2_ttf)
+
+FetchContent_MakeAvailable(SDL2 SDL2_ttf)
